d0f54c86161e6be2ff8ebdcdf95330687fe3cc2a
Ryan Berliner <ryan.berliner@gmail.com>
2020-03-21 19:08:08 -0400

    fixes bug where elements with gravity cant be close to each other

--- gravity-cursor.js
+++ gravity-cursor.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   var body = document.querySelector("body");
-  var currentlyOrbiting = null;
+
+  var current = -1;
+  var prev = -1;
+
   var orbitTransforms = {x: 0,y: 0};
   var pullDistance = 0;
   var cursorSize = 25;
@@ -41,90 +44,100 @@ document.addEventListener("DOMContentLoaded", function() {
   // Listen for mouse moves
   var lastMove = 0;
   document.addEventListener("mousemove", function(event) {
-    if (Date.now() - lastMove < 10) {
-      return;
-    }
-
+    // Limit number of updates.
+    if (Date.now() - lastMove < 10) return;
     lastMove = Date.now();
 
+    // Mark our first update
     if (!gravityActivated) {
       body.classList.add('gravity-activated');
       gravityActivated = true;
     }
 
+    // Position the cursor
     var cursorX = event.clientX - (cursor.offsetWidth / 2);
     var cursorY = event.clientY - (cursor.offsetHeight / 2);
     cursor.style.left = cursorX + 'px';
     cursor.style.top = cursorY + 'px';
 
-    var hasOrbit = false;
-    var transformRules;
+    // Keep track of whether we are in orbit
+    var isInOrbit = false;
 
-    // Check if we are in the gravitational pull of any elements
+    // Check if we should be orbitting any elements
     for (var i = 0; i < gravitationals.length; i++) {
       var pullBounding = gravitationals[i].pullBounding;
       if (pullBounding.top <= cursorY && pullBounding.bottom >= cursorY && pullBounding.left <= cursorX && pullBounding.right >= cursorX) {
-        currentlyOrbiting = gravitationals[i].node;
+        // In orbit
+        isInOrbit = true;
+
+        // New current, keep track of the prev as well
+        if (i !== current) {
+          prev = current;
+          current = i;
+          setTimeout(function() {
+            element.classList.add('been-awhile');
+          }, 300);
+        }
+
+        var element = gravitationals[current].node;
+
+        // If we are transforming, and we haven't been here for a while just wait.
+        if (element.style.transform && !element.classList.contains('been-awhile')) {
+          break;
+        }
 
         // Calculate transforms
         var width = gravitationals[i].bounding.width + (2*pullDistance);
         var height = gravitationals[i].bounding.height + (2*pullDistance);
         var center = {x: pullBounding.right - (width/2), y: pullBounding.bottom - (height/2 + cursorSize/2)};
 
-        if (!currentlyOrbiting.style.transform || currentlyOrbiting.classList.contains('been-awhile')) {
-          perfectOrbitTransforms = {
-            x: ((cursorX-center.x)/width)*2,
-            y: ((cursorY-center.y)/height)*10 // Have more of an effect in the y direciton
-          }
-          if (currentlyOrbiting.style.transform) {
-            // Lets bias the current ones to create smooth movements
-            orbitTransforms = {
-              x: (orbitTransforms.x * 9 + perfectOrbitTransforms.x * 1) / 10,
-              y: (orbitTransforms.y * 9 + perfectOrbitTransforms.y * 1) / 10
-            }
-          } else {
-            orbitTransforms = perfectOrbitTransforms;
-          }
-
-          transformRules = `translate(${orbitTransforms.x}%, ${orbitTransforms.y}%) scale(1.07)`;
+        perfectOrbitTransforms = {
+          x: ((cursorX-center.x)/width)*3,
+          y: (cursorY-center.y)/height*7
         }
 
-        hasOrbit = true;
+        if (element.style.transform) {
+          // Lets bias the current ones to create smooth movements
+          orbitTransforms = {
+            x: (orbitTransforms.x * 9 + perfectOrbitTransforms.x * 1) / 10,
+            y: (orbitTransforms.y * 9 + perfectOrbitTransforms.y * 1) / 10
+          }
+        } else {
+          orbitTransforms = perfectOrbitTransforms;
+        }
+
+        element.style.transform = `translate(${orbitTransforms.x}%, ${orbitTransforms.y}%) scale(1.07)`;
+
+        if (!element.classList.contains('currently-orbitting')) {
+          element.classList.add('currently-orbitting');
+        }
+
+        // Don't bother looking anymore
+        break;
       }
     }
 
-    // Adjust classes as needed.
-    if (!hasOrbit && currentlyOrbiting != null) {
-      currentlyOrbiting.classList.remove('currently-orbitting');
-      currentlyOrbiting.classList.remove('been-awhile');
-      currentlyOrbiting.style.transform = '';
-      currentlyOrbiting.blur();
-      currentlyOrbiting = null;
+    if (!isInOrbit && current !== -1) {
+      prev = current;
+      current = -1;
+    }
+    
+    // Remove things from previously orbitted element.
+    if (prev !== -1) {
+      var prevElement = gravitationals[prev].node;
+      if (prevElement != null && (prevElement.classList.contains('currently-orbitting') || prevElement.classList.contains('been-awhile'))) {
+        prevElement.classList.remove('currently-orbitting');
+        prevElement.classList.remove('been-awhile');
+        prevElement.style.transform = '';
+      }
     }
 
-    if (hasOrbit && !body.classList.contains('orbitting')) {
+    if (isInOrbit && !body.classList.contains('orbitting')) {
       body.classList.add('orbitting');
     }
 
-    if (!hasOrbit && body.classList.contains('orbitting')) {
+    if (!isInOrbit && body.classList.contains('orbitting')) {
       body.classList.remove('orbitting');
-    }
-
-    if (currentlyOrbiting != null && !currentlyOrbiting.classList.contains('currently-orbitting')) {
-      currentlyOrbiting.classList.add('currently-orbitting');
-      var timeout = setTimeout(function() {
-        if (currentlyOrbiting != null) {
-          currentlyOrbiting.classList.add('been-awhile');
-        }
-      }, 300);
-      currentlyOrbiting.addEventListener('mouseleave', function() {
-        clearTimeout(timeout);
-      });
-    }
-
-    if (currentlyOrbiting != null) {
-      currentlyOrbiting.style.transform = transformRules;
-      currentlyOrbiting.focus();
     }
   });
   
